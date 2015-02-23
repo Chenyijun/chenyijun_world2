9f257acd49350f0a23755613db8f138f6ad248f8
Raksha Rao <srir@andrew.cmu.edu>
2015-02-22 21:09:16 -0500

    Created branch articles, set up model with scopes, relationships, and validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,11 @@
 class Category < ActiveRecord::Base
+
+  #Relationships
   has_many :photos
+  has_many :articles
   
+  #Scopes
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
+  
 end
